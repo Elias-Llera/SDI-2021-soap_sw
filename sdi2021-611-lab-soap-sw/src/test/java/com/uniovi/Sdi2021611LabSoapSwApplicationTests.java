a24507d43582c39c79464740b1497bedb76a5648
Elias-Llera <eliaslleragr@gmail.com>
2021-04-24 12:58:18 +0200

    Set up del proyecto

---
+++ sdi2021-611-lab-soap-sw/src/test/java/com/uniovi/Sdi2021611LabSoapSwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2021611LabSoapSwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
